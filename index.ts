11abd2756f88f110b2e6734f23591204e3d50c50
Jon Van Steen <jvansteen1@gmail.com>
2026-02-12 08:38:14 -0500

    fix finding commands

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ client.commands = new Collection();
 const commandsPath = path.join(__dirname, "src", "commands");
 const commandFiles = fs
   .readdirSync(commandsPath)
-  .filter((file) => file.endsWith(".ts"));
+  .filter((file) => file.endsWith(".ts") || file.endsWith('js'));
 
 for (const file of commandFiles) {
   const filePath = path.join(commandsPath, file);
